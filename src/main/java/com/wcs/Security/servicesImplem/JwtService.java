24bb70883f3870b708be32e0909a7ae8e95d5dce
msykhanou123 <m.sykhanou@gmail.com>
2023-07-03 16:51:45 +0200

    Fix Expiration in JwtService

--- src/main/java/com/wcs/Security/servicesImplem/JwtService.java
+++ src/main/java/com/wcs/Security/servicesImplem/JwtService.java
@@ -42,6 +42,7 @@ public class JwtService {
                 .setClaims(extraClaims)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
+                // Set expiration time to 24 hours
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
